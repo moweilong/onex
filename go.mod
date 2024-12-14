459c65dfa6ea6b294a10fd17e945cd5260cecbb8
moweilong <18550039021@163.com>
2024-12-14 09:57:33 +0800

    feat: 认证工具封装，并提供Kratos的中间件

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/moweilong/onex
+
+go 1.23.4
+
+require (
+	github.com/go-kratos/kratos/v2 v2.8.2
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+	github.com/stretchr/testify v1.7.0
+	google.golang.org/grpc v1.69.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-playground/form/v4 v4.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241015192408-796eee8c2d53 // indirect
+	google.golang.org/protobuf v1.35.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ pkg/authn/engine/jwt/jwt.go
@@ -0,0 +1,141 @@
+package jwt
+
+import (
+	"context"
+	"errors"
+
+	jwtV5 "github.com/golang-jwt/jwt/v5"
+
+	"github.com/moweilong/onex/pkg/authn/engine"
+	"github.com/moweilong/onex/pkg/authn/engine/utils"
+)
+
+var _ engine.Authenticator = (*Authenticator)(nil)
+
+type Authenticator struct {
+	options *Options
+}
+
+func NewAuthenticator(opts ...Option) (engine.Authenticator, error) {
+	auth := &Authenticator{
+		options: &Options{},
+	}
+
+	for _, o := range opts {
+		o(auth.options)
+	}
+
+	if auth.options.signingMethod == nil {
+		auth.options.signingMethod = jwtV5.SigningMethodHS256
+	}
+
+	return auth, nil
+}
+
+// Authenticate authenticates the token string and returns the claims.
+func (jwt *Authenticator) Authenticate(ctx context.Context, contextType engine.ContextType) (*engine.AuthClaims, error) {
+	tokenString, err := utils.AuthFromMD(ctx, utils.BearerWord, contextType)
+	if err != nil {
+		return nil, engine.ErrMissingBearerToken
+	}
+
+	return jwt.AuthenticateToken(tokenString)
+}
+
+// AuthenticateToken authenticates the token string and returns the claims.
+func (jwt *Authenticator) AuthenticateToken(tokenString string) (*engine.AuthClaims, error) {
+	jwtToken, err := jwt.parseToken(tokenString)
+
+	if jwtToken == nil {
+		return nil, engine.ErrInvalidToken
+	}
+
+	if err != nil {
+		switch {
+		case errors.Is(err, jwtV5.ErrTokenMalformed):
+			return nil, engine.ErrInvalidToken
+		case errors.Is(err, jwtV5.ErrTokenSignatureInvalid):
+			return nil, engine.ErrSignTokenFailed
+		case errors.Is(err, jwtV5.ErrTokenExpired) || errors.Is(err, jwtV5.ErrTokenNotValidYet):
+			return nil, engine.ErrTokenExpired
+		default:
+			return nil, engine.ErrInvalidToken
+		}
+	}
+
+	if !jwtToken.Valid {
+		return nil, engine.ErrInvalidToken
+	}
+	if jwtToken.Method != jwt.options.signingMethod {
+		return nil, engine.ErrUnsupportedSigningMethod
+	}
+	if jwtToken.Claims == nil {
+		return nil, engine.ErrInvalidClaims
+	}
+
+	claims, ok := jwtToken.Claims.(jwtV5.MapClaims)
+	if !ok {
+		return nil, engine.ErrInvalidClaims
+	}
+
+	authClaims, err := utils.MapClaimsToAuthClaims(claims)
+	if err != nil {
+		return nil, err
+	}
+
+	return authClaims, nil
+}
+
+// CreateIdentityWithContext creates a signed token string from the claims and sets it to the context.
+func (jwt *Authenticator) CreateIdentityWithContext(ctx context.Context, contextType engine.ContextType, claims engine.AuthClaims) (context.Context, error) {
+	strToken, err := jwt.CreateIdentity(claims)
+	if err != nil {
+		return ctx, err
+	}
+
+	ctx = utils.MDWithAuth(ctx, utils.BearerWord, strToken, contextType)
+
+	return ctx, nil
+}
+
+// CreateIdentity creates a signed token string from the claims.
+func (jwt *Authenticator) CreateIdentity(claims engine.AuthClaims) (string, error) {
+	jwtToken := jwtV5.NewWithClaims(jwt.options.signingMethod, utils.AuthClaimsToJwtClaims(claims))
+
+	strToken, err := jwt.generateToken(jwtToken)
+	if err != nil {
+		return "", err
+	}
+
+	return strToken, nil
+}
+
+func (jwt *Authenticator) Close() {}
+
+// parseToken parses the token string and returns the token.
+func (jwt *Authenticator) parseToken(token string) (*jwtV5.Token, error) {
+	if jwt.options.keyFunc == nil {
+		return nil, engine.ErrMissingKeyFunc
+	}
+
+	return jwtV5.Parse(token, jwt.options.keyFunc)
+}
+
+// generateToken generates a signed token string from the token.
+func (jwt *Authenticator) generateToken(jwtToken *jwtV5.Token) (string, error) {
+	if jwt.options.keyFunc == nil {
+		return "", engine.ErrMissingKeyFunc
+	}
+
+	key, err := jwt.options.keyFunc(jwtToken)
+	if err != nil {
+		return "", engine.ErrGetKeyFailed
+	}
+
+	strToken, err := jwtToken.SignedString(key)
+	if err != nil {
+		return "", engine.ErrSignTokenFailed
+	}
+
+	return strToken, nil
+}
